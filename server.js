b9708b187ed3840952c625ae47c23d32ce641cce
Tony Tran <codemonkey713@gmail.com>
2021-04-01 18:05:09 -0500

    added framework for express server barebone

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8080;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", { useNewUrlParser: true });
+
+
+
+app.listen(PORT, () => {
+  console.log(`Fitness Tracker Application running on port ${PORT}!`);
+});
